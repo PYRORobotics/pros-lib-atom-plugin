7e304ce0d01479c97c641f96979d69eb77f56c50
Brandon Rice <bjrice13@gmail.com>
2018-09-17 14:12:22 -0700

    First Commit

---
+++ spec/pros-lib-atom-plugin-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import ProsLibAtomPluginView from '../lib/pros-lib-atom-plugin-view';
+
+describe('ProsLibAtomPluginView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
